a1b4fab864903c61f6fda577c3192c899191b786
Константин Дьяченко <konard@me.com>
2019-08-30 20:39:35 +0300

    Today no comments, so let's ignore warnings for a while.

--- Comparer.cs
+++ Comparer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
 namespace Platform.Comparers
 {
     public class Comparer : IComparer
